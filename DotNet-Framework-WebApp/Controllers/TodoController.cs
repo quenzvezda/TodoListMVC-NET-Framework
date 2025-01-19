8e8acd6e8c7cc9a964f001cbc65a9933ff8ac10b
quenzvezda <quenzvezda@gmail.com>
2025-01-19 13:37:03 +0700

    Create Todo Controller & Service

---
+++ DotNet-Framework-WebApp/Controllers/TodoController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Web.Mvc;
+using DotNet_Framework_WebApp.Models;
+using DotNet_Framework_WebApp.Services;
+
+namespace DotNet_Framework_WebApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly TodoService _todoService;
+
+        public TodoController()
+        {
+            var context = new TodoItemContext();
+            _todoService = new TodoService(context);
+        }
+        
+        // GET: Todo
+        public ActionResult Index()
+        {
+            var todoItems = _todoService.GetAllTodoItems();
+            return View(todoItems);
+        }
+
+        // GET: Todo/PreAdd
+        public ActionResult PreAdd()
+        {
+            return View();
+        }
+
+        // POST: Todo/Add
+        [HttpPost]
+        public ActionResult Add(string title)
+        {
+            if (!string.IsNullOrEmpty(title))
+            {
+                var newTodo = new TodoItem
+                {
+                    Title = title,
+                    CreatedDate = DateTime.Now,
+                    IsComplete = false
+                };
+                _todoService.AddTodoItem(newTodo);
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Todo/PreEdit/{id}
+        public ActionResult PreEdit(int id)
+        {
+            var todoItem = _todoService.GetTodoById(id);
+            if (todoItem == null) return HttpNotFound();
+
+            return View(todoItem);
+        }
+
+        // POST: Todo/Edit
+        [HttpPost]
+        public ActionResult Edit(int id, string title, bool isComplete)
+        {
+            var todoItem = _todoService.GetTodoById(id);
+            if (todoItem != null)
+            {
+                todoItem.Title = title;
+                todoItem.IsComplete = isComplete;
+                todoItem.UpdatedDate = DateTime.Now;
+
+                if (isComplete)
+                {
+                    todoItem.FinishDate = DateTime.Now;
+                }
+
+                _todoService.UpdateTodoItem(todoItem);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Todo/Delete/{id}
+        public ActionResult Delete(int id)
+        {
+            _todoService.DeleteTodoItem(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DotNet-Framework-WebApp/Services/TodoService.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DotNet_Framework_WebApp.Models;
+
+namespace DotNet_Framework_WebApp.Services
+{
+    public class TodoService
+    {
+        private readonly TodoItemContext _context;
+
+        public TodoService(TodoItemContext context)
+        {
+            _context = context;
+        }
+        
+        public List<TodoItem> GetAllTodoItems()
+        {
+            return _context.TodoItems.ToList();
+        }
+        
+        public TodoItem GetTodoById(int id)
+        {
+            return _context.TodoItems.Find(id);
+        }
+
+        public void AddTodoItem(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            _context.SaveChanges();
+        }
+
+        public void UpdateTodoItem(TodoItem item)
+        {
+            _context.Entry(item).State = System.Data.Entity.EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void DeleteTodoItem(int id)
+        {
+            var item = _context.TodoItems.Find(id);
+            if (item != null)
+            {
+                _context.TodoItems.Remove(item);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
